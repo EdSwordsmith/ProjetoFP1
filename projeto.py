2edfc3f6af53f16794d7d5865675ab25da3c8409
Eduardo Espadeiro <eduardo.espadeiro@tecnico.ulisboa.pt>
2019-10-30 22:58:14 +0000

    comentarios e uma funcao auxiliar

--- projeto.py
+++ projeto.py
@@ -4,15 +4,15 @@
 def eh_labirinto(tuplo):
     """
     Devolve True se tuplo for um labirinto.
-    Um labirinto e um tuplo que contem nx tuplos, cada um
-    contem ny inteiros 0 ou 1.
+    Um labirinto e um tuplo que contem nx tuplos,
+    cada um contem ny inteiros 1 ou 0.
     """
-    if not type(tuplo) is tuple:
+    if not type(tuplo) is tuple or len(tuplo) < 3 or len(tuplo[0]) < 3:
         return False
 
-    nx, ny = len(tuplo), 0
+    nx, ny = len(tuplo), len(tuplo[0])
     for i in range(nx):
-        if not type(tuplo[i]) is tuple or (ny != len(tuplo[i]) and i != 0):
+        if type(tuplo[i]) is not tuple or len(tuplo[i]) != ny:
             return False
 
         for j in range(len(tuplo[i])):
@@ -21,21 +21,17 @@ def eh_labirinto(tuplo):
                     or ((i == 0 or i == nx - 1) and tuplo[i][j] != 1) \
                     or ((j == 0 or j == ny) and tuplo[i][j] != 1):
                 return False
-
-        ny = len(tuplo[i])
-
-    return nx >= 3 and ny >= 3
+    return True
 
 
 def eh_posicao(tuplo):
     """
-    Devolve True se pos for uma posicao.
-    Uma posicao e um tuplo que contem 2 inteiros nao negativos
+    Devolve True se tuplo for uma posicao.
+    Uma posicao e um tuplo que contem 2 inteiros nao negativos.
     """
-    return \
-        type(tuplo) is tuple and len(tuplo) == 2 \
-        and tuplo[0] >= 0 and tuplo[1] >= 0 \
-        and type(tuplo[0]) is int and type(tuplo[1]) is int
+    return type(tuplo) is tuple and len(tuplo) == 2 \
+        and type(tuplo[0]) is int and type(tuplo[1]) is int \
+        and tuplo[0] >= 0 and tuplo[1] >= 0
 
 
 def eh_conj_posicoes(tuplo):
@@ -43,14 +39,14 @@ def eh_conj_posicoes(tuplo):
     Devolve True se tuplo for um conjunto de posicoes.
     Um conjunto de posicoes e um tuplo que contem tuplos que verifiquem eh_posicao.
     """
-    if not type(tuplo) is tuple:
+    if type(tuplo) is not tuple:
         return False
 
-    elementos = []
+    elementos = ()
     for el in tuplo:
         if not eh_posicao(el) or el in elementos:
             return False
-        elementos.append(el)
+        elementos = elementos + (el,)
     return True
 
 
@@ -61,6 +57,16 @@ def tamanho_labirinto(labirinto):
     if not eh_labirinto(labirinto):
         raise ValueError("tamanho_labirinto: argumento invalido")
     return (len(labirinto), len(labirinto[0]))
+
+
+def eh_posicao_valida(labirinto, posicao):
+    """
+    Devolve True se posicao se encontrar dentro do labirinto
+    e nao existir paredes nessa posicao.
+    """
+    nx, ny = tamanho_labirinto(labirinto)
+    x, y = posicao
+    return 0 <= x < nx and 0 <= y <= ny and labirinto[x][y] == 0
 
 
 def eh_mapa_valido(labirinto, unidades):
@@ -74,8 +80,7 @@ def eh_mapa_valido(labirinto, unidades):
 
     nx, ny = tamanho_labirinto(labirinto)
     for pos in unidades:
-        if pos[0] >= nx or pos[0] < 0 or pos[1] >= ny \
-                or pos[1] < 0 or labirinto[pos[0]][pos[1]] == 1:
+        if not eh_posicao_valida(labirinto, pos):
             return False
     return True
 
@@ -88,15 +93,10 @@ def eh_posicao_livre(labirinto, unidades, posicao):
             or not eh_mapa_valido(labirinto, unidades):
         raise ValueError("eh_posicao_livre: algum dos argumentos e invalido")
 
-    nx, ny = tamanho_labirinto(labirinto)
-    if posicao[0] >= nx or posicao[0] < 0 or posicao[1] >= ny \
-            or posicao[1] < 0 or labirinto[posicao[0]][posicao[1]] == 1:
+    if not eh_posicao_valida(labirinto, posicao):
         return False
 
-    for p in unidades:
-        if p[0] == posicao[0] and p[1] == posicao[1]:
-            return False
-    return True
+    return posicao not in unidades
 
 
 def posicoes_adjacentes(posicao):
@@ -127,7 +127,7 @@ def mapa_str(labirinto, unidades):
         for x in range(len(labirinto)):
             if labirinto[x][y] == 1:
                 res = res + "#"
-            elif not eh_posicao_livre(labirinto, unidades, (x, y)):
+            elif (x, y) in unidades:
                 res = res + "O"
             else:
                 res = res + "."
@@ -142,7 +142,7 @@ def obter_objetivos(labirinto, unidades, posicao):
     a unidade correspondente a pos.
     """
     if not eh_labirinto(labirinto) or not eh_conj_posicoes(unidades) or not eh_posicao(posicao) \
-        or posicao not in unidades or not eh_mapa_valido(labirinto, unidades):
+            or posicao not in unidades or not eh_mapa_valido(labirinto, unidades):
         raise ValueError("obter_objetivos: algum dos argumentos e invalido")
     res = ()
     for unidade in unidades:
@@ -166,10 +166,12 @@ def obter_caminho(labirinto, unidades, posicao):
     visitadas = []
     objetivos = obter_objetivos(labirinto, unidades, posicao)
 
+    # Verificar se a posicao inicial se encontra numa posicao adjacente a alguma unidade
     for unidade in unidades:
         if posicao in posicoes_adjacentes(unidade):
             return (posicao,)
 
+    # Algoritmo BFS
     while fila_exploracao:
         caminho_atual = fila_exploracao.pop(0)
         posicao_atual = caminho_atual[-1]
@@ -186,13 +188,17 @@ def obter_caminho(labirinto, unidades, posicao):
 
 
 def mover_unidade(labirinto, unidades, posicao):
+    """
+    Devolve um conjunto de posicoes correspondente as unidades depois de
+    realizado um movimento sobre uma das unidades.
+    """
     if not eh_labirinto(labirinto) or not eh_conj_posicoes(unidades) or not eh_posicao(posicao) \
             or posicao not in unidades or not eh_mapa_valido(labirinto, unidades):
         raise ValueError("mover_unidade: algum dos argumentos e invalido")
 
     caminho = obter_caminho(labirinto, unidades, posicao)
 
-    if len(unidades) > 1 and len(caminho) > 1:
+    if len(caminho) > 1:
         for i in range(len(unidades)):
             if unidades[i] == posicao:
                 return unidades[:i] + (caminho[1],) + unidades[i+1:]
